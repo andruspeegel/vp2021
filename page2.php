11f330919876618fa0f5be783e6f327d86ea6185
Andrus Peegel <andruspeegel@users.noreply.github.com>
2021-09-21 15:43:39 +0300

    Kolmas kodune töö

--- page2.php
+++ page2.php
@@ -16,6 +16,7 @@
 		}
 	}
 	//Juhusliku foto lisamine
+	$photo_num = null;
 	$photo_dir = "photos/";
 	//Loen kataloogi sisu
 	$all_files = scandir($photo_dir);
@@ -37,12 +38,36 @@
 	$file_count = count($photo_files);
 	//Loosin juhusliku arvu (minimaalne peab olema 0 ja max count -1)
 	$photo_num = mt_rand(0, $file_count - 1);
-	//img src="kataloog/fail" alt="Tallinna Ülikool">
-	$photo_html = '<img src="' .$photo_dir .$photo_files[$photo_num] .'" alt="Talinna Ülikool" width="400">';
 	
-	$photo_select_html = "\n" .'<select name="photo_select">' ."\n";
-	for($i = 0;$i < $file_count;$i ++){
-		$photo_select_html .= '<option value="' .$i .'">' .$photo_files[$i] ."</option> \n"; 
+	if(isset($_POST["photo_select_submit"])){
+		$photo_num = $_POST["photo_select"];
+	}
+	
+	$photo_file_html = null;
+	$photo_file = $photo_files[$photo_num];
+	$photo_html = '<img src="' .$photo_dir .$photo_file .'" alt="Talinna Ülikool" width="400">';
+	
+	$photo_file_html = "\n <p>".$photo_file ."</p> \n";
+	
+	$photo_list_html = "<ul> \n";
+	for($i = 0; $i < $file_count; $i ++){
+		$photo_list_html .= "<li>" .$photo_files[$i] ."</li> \n";
+	}
+	$photo_list_html .= "</ul>";
+	
+	$photo_file_html = null;
+	$photo_file = $photo_files[$photo_num];
+	$photo_html = '<img src="' .$photo_dir .$photo_file .'" alt="Tallinna Ülikool">';
+	
+	$photo_file_html = "\n <p>".$photo_file ."</p> \n";
+	
+	$photo_select_html = '<select name="photo_select">' ."\n";
+	for($i = 0; $i < $file_count; $i ++){
+		$photo_select_html .= "\t \t" .'<option value="' .$i .'"';
+		if($i == $photo_num){
+			$photo_select_html .= " selected";
+		}
+		$photo_select_html .= ">" .$photo_files[$i] ."</option> \n";
 	}
 	$photo_select_html .= "</select> \n";
 	
@@ -67,10 +92,14 @@
 	<hr>
 	<form method="POST">
 		<?php echo $photo_select_html; ?>
+		<input type="submit" name="photo_select_submit" value="Näita valitud fotot">
 	</form>
 	<hr>
 	<?php 
 		echo $photo_html; 
+		echo $photo_file_html;
+		echo "<hr> \n";
+		echo "<hr> \n";
 		echo $photo_list_html; 
 	?>
 </body>
